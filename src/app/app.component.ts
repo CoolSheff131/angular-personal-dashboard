3c62c6b0cee5b9cb10cd64181e51e8640e3c100d
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-06-25 22:53:05 +0800

    animation for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,132 @@
+import {
+  animate,
+  group,
+  query,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routeAnim', [
+      transition(':increment', [
+        style({
+          position: 'relative',
+          overflow: 'hidden',
+        }),
+        query(
+          ':enter, :leave',
+          [
+            style({
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+            }),
+          ],
+          { optional: true }
+        ),
+
+        group([
+          query(
+            ':leave',
+            [
+              animate(
+                '200ms ease-in',
+                style({
+                  opacity: 0,
+                  transform: 'translateX(-80px)',
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+
+          query(
+            ':enter',
+            [
+              style({
+                transform: 'translateX(80px)',
+                opacity: 0,
+              }),
+              animate(
+                '250ms 120ms ease-out',
+                style({
+                  opacity: 1,
+                  transform: 'translateX(0px)',
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+        ]),
+      ]),
+      transition(':decrement', [
+        style({
+          position: 'relative',
+          overflow: 'hidden',
+        }),
+        query(
+          ':enter, :leave',
+          [
+            style({
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+            }),
+          ],
+          { optional: true }
+        ),
+
+        group([
+          query(
+            ':leave',
+            [
+              animate(
+                '200ms ease-in',
+                style({
+                  opacity: 0,
+                  transform: 'translateX(80px)',
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+
+          query(
+            ':enter',
+            [
+              style({
+                transform: 'translateX(-80px)',
+                opacity: 0,
+              }),
+              animate(
+                '250ms 120ms ease-out',
+                style({
+                  opacity: 1,
+                  transform: 'translateX(0px)',
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'angular-personal-dashboard';
+  prepareRoute(outlet: RouterOutlet) {
+    if (outlet.isActivated) return outlet.activatedRouteData[`tab`];
+    return;
+  }
 }
