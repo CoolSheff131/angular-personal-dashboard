934de040696721703c9f1bddd15b879c12450823
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-07-05 19:00:54 +0800

    add todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NoteCardComponent } from './note-card/note-card.component';
 import { FormsModule } from '@angular/forms';
 import { EditNoteComponent } from './edit-note/edit-note.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { TodoItemComponent } from './todo-item/todo-item.component';
     NoteCardComponent,
     EditNoteComponent,
     TodoItemComponent,
+    AddTodoComponent,
   ],
   imports: [
     BrowserModule,
