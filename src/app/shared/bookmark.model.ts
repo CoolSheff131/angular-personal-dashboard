ddc90385fcaf11113dec17375154f8594277b675
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-07-18 17:36:59 +0800

    add bookmark

--- src/app/shared/bookmark.model.ts
+++ src/app/shared/bookmark.model.ts
@@ -2,10 +2,16 @@ import { v4 as uuidv4 } from 'uuid';
 
 export class Bookmark {
   id: string;
-
+  name: string;
   url: URL;
-  constructor(public name: string, url: string) {
+  constructor(name: string, url: string) {
     this.url = new URL(url);
     this.id = uuidv4();
+
+    if (!name) {
+      name = this.url.hostname;
+    }
+
+    this.name = name;
   }
 }
