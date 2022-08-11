b5b4dc4502077f73b1e3d28d0f82bfec40092598
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-08-11 20:11:19 +0800

    change background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -122,11 +122,72 @@ import { RouterOutlet } from '@angular/router';
         ]),
       ]),
     ]),
+
+    trigger('bgAnim', [
+      transition(':leave', [
+        animate(
+          1000,
+          style({
+            opacity: 0,
+          })
+        ),
+      ]),
+    ]),
+
+    trigger('fadeAnim', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(
+          250,
+          style({
+            opacity: 1,
+          })
+        ),
+      ]),
+
+      transition(':leave', [
+        animate(
+          250,
+          style({
+            opacity: 1,
+          })
+        ),
+      ]),
+    ]),
   ],
 })
 export class AppComponent {
+  backgrounds: string[] = [
+    'https://images.unsplash.com/photo-1655252205276-c4e210f36333?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1NTYxODIyMQ&ixlib=rb-1.2.1&q=80&w=1920',
+  ];
+
+  loadingBGImage = false;
+
   prepareRoute(outlet: RouterOutlet) {
     if (outlet.isActivated) return outlet.activatedRouteData[`tab`];
     return;
+  }
+
+  onBGImageLoad(imgEvent: Event) {
+    const imgElement = imgEvent.target as HTMLImageElement;
+    const src = imgElement.src;
+    this.backgrounds = this.backgrounds.filter((b) => b === src);
+
+    this.loadingBGImage = false;
+  }
+
+  async changeBGImage(): Promise<void> {
+    this.loadingBGImage = true;
+    const result = await fetch('https://source.unsplash.com/random/1920x1080', {
+      method: 'HEAD',
+    });
+
+    const alreadyGot = this.backgrounds.includes(result.url);
+
+    if (alreadyGot) {
+      return this.changeBGImage();
+    }
+
+    this.backgrounds.push(result.url);
   }
 }
